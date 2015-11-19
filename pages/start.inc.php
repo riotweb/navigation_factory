88ee23da530d314761695d8ae083c4edb5ed93bc
RexDude <rexdude@arcor.de>
2015-11-19 09:33:12 +0100

    Added startpage 3

--- pages/start.inc.php
+++ pages/start.inc.php
@@ -2,6 +2,5 @@
 	<h2 class="rex-hl2"><?php echo $I18N->msg('navigation_factory_start_title'); ?></h2>
 	<div class="rex-area-content">
 		<p class="logo"><img src="../<?php echo $REX['MEDIA_ADDON_DIR']; ?>/navigation_factory/logo.png" width="220" height="270" alt="" /></p>
-		<br />
 	</div>
 </div>
